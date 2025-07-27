5970476314484e04374095db45b00e4b782267f6
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-07-27 19:04:16 +0530

    Code Block Padding

--- src/components/ui/Conversation.tsx
+++ src/components/ui/Conversation.tsx
@@ -28,11 +28,11 @@ const Conversation = ({activeMessages, isThinking}:ConversationPropType) => {
         return <div key={index + message._id} className={cn("flex",message.role === "user" ? "justify-end" : "justify-start")}>
 
                   {/* Bubble Container */}
-                  <div className="relative group max-w-[85%] px-4 py-2 mb-2">
+                  <div className="relative group max-w-[85%] px-4 p-2 mb-2">
      
                     <MarkdownPreview
                       source={message.content}
-                      className="rounded-lg px-4 py-2 mb-2 whitespace-pre-wrap"
+                      className="rounded-lg p-2 mb-2 whitespace-pre-wrap"
                       style={{
                         backgroundColor: message.role === "user" ? "var(--markdown-user-background)" : "var(--markdown-assistant-background)",
                         color: message.role === "user" ? "var(--markdown-user-text)" : "var(--markdown-assistant-text)",
