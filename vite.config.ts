1382a6e977e8bc848b8e42bd65c6ebf38f425229
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-09-05 12:54:01 +0530

    loading VITE_BACKEND_HOST

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 import { resolve } from 'node:path'
 
-const BACKEND_HOST = process.env.NODE_ENV  == "production" ?  process.env.VITE_BACKEND_HOST : "http://localhost:8080";
+export default defineConfig(({ mode }) => {
+  // Load env file based on the current mode (development/production)
+  const env = loadEnv(mode, process.cwd(), '')
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [viteReact(), tailwindcss()],
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: BACKEND_HOST,
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+  const BACKEND_HOST =
+    mode === 'production'
+      ? env.VITE_BACKEND_HOST
+      : 'http://localhost:8080'
+
+  console.log('Using BACKEND_HOST =', BACKEND_HOST)
+
+  return {
+    plugins: [viteReact(), tailwindcss()],
+    server: {
+      host: true,
+      proxy: {
+        '/api': {
+          target: BACKEND_HOST,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+  }
 })
