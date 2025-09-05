fd40bea1392b49d709a7dea227bf1705ef9ddf9e
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-09-05 12:24:39 +0530

    Constants Updated

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,5 @@
 const NODE_ENV = import.meta.env.MODE ||  "development";
-export const BACKEND_HOST = NODE_ENV == "production" ? "http://localhost:8080" : "";
-export const FRONTEND_HOST = NODE_ENV == "production" ? "http://localhost:5173" : "https://querynox.xyz";
+export const BACKEND_HOST = NODE_ENV == "production" ? import.meta.env.BACKEND_HOST : "http://localhost:8080";
+export const FRONTEND_HOST = NODE_ENV == "production" ? import.meta.env.FRONTEND_HOST : "http://localhost:5173";
 export const SohamJoshiGithubLink = "https://github.com/sohamjoshi25";
 export const Hackice20GithubLink = "https://github.com/hackice20";
