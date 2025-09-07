294af67df2574a9785f670618a67e05c10d16a1b
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-09-07 23:40:56 +0530

    env fix?

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,6 +1,6 @@
-export const NODE_ENV = import.meta.env.MODE || "development";
+export const NODE_ENV = (import.meta.env.MODE || "development") as string;
 
-export const BACKEND_HOST = NODE_ENV === "production" ? import.meta.env.VITE_BACKEND_HOST : "http://localhost:8080";
-export const FRONTEND_HOST = import.meta.env.VITE_SERVER_URL || "http://localhost:5173";
-export const SohamJoshiGithubLink = "https://github.com/sohamjoshi25";
-export const Hackice20GithubLink = "https://github.com/hackice20";
+export const BACKEND_HOST = (NODE_ENV == "production" ? ( import.meta.env.VITE_BACKEND_HOST  || "https://backend.querynox.xyz" ) : "http://localhost:8080") as string;
+export const FRONTEND_HOST =  (NODE_ENV == "production" ? ( import.meta.env.VITE_FRONTEND_HOST || "https://www.querynox.xyz" ) : "http://localhost:5173") as string;
+export const SohamJoshiGithubLink = "https://github.com/sohamjoshi25" as string;
+export const Hackice20GithubLink = "https://github.com/hackice20" as string;
